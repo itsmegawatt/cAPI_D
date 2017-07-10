066858b290ac93c8b0120d6b953b89700fedec02
TaukeerAhmad <taukeer.ahmad@3pillarglobal.com>
2017-07-10 19:55:48 +0530

    Java Code for Form API
    This is the java version of FORM API call.

---
+++ Java Code/FormAPI.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FormAPI {
+
+	public String call(String url, String username, String password) {
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		try {
+			String apiUrl = url + "?username=" + username + "&password="
+					+ password;
+			URL obj = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("GET");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String l = null;
+			while (( l=br.readLine()) != null) {
+				stringBuilder.append(l);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return stringBuilder.toString();
+	}
+
+	public static void main(String[] args) {
+
+		FormAPI formAPI = new FormAPI();
+		String url = "https://www.gocanvas.com/apiv2/forms.xml";
+		String username = "taukeerahmed.knmiet@gmail.com";
+		String password = "abc123";
+		String outputXml=formAPI.call(url, username, password);
+		System.out.println("xml response: "+outputXml);
+
+	}
+
+}
