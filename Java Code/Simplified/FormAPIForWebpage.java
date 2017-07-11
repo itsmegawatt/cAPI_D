c60a18200afe8a9455079cda724d963179e3e380
TaukeerAhmad <taukeer.ahmad@3pillarglobal.com>
2017-07-11 18:00:23 +0530

    added simplified java code for bootstrap webpage

---
+++ Java Code/Simplified/FormAPIForWebpage.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FormAPIForWebpage {
+	public static void main(String[] args) {
+		StringBuilder stringBuilder = new StringBuilder();
+		try {
+			String apiUrl = "https://www.gocanvas.com/apiv2/forms.xml?username=taukeerahmed.knmiet@gmail.com&password=abc123";
+			URL obj = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("GET");
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String l = null;
+			while ((l = br.readLine()) != null) {
+				stringBuilder.append(l);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("xml response: " + stringBuilder);
+	}
+}
