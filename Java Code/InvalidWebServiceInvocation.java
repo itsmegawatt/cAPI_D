6358659c9f7f5f87effc88c7f7353ac64cea596f
TaukeerAhmad <taukeer.ahmad@3pillarglobal.com>
2017-07-13 13:53:48 +0530

    Added one more API and done formatting for already commit API Code. Also changed userId and password to default.

---
+++ Java Code/InvalidWebServiceInvocation.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class InvalidWebServiceInvocation {
+
+	public String call(String url, String username, String password) {
+
+		StringBuilder stringBuilder = new StringBuilder();
+		try {
+			String apiUrl = url + "?username=" + username + "&password="
+					+ password;
+			URL obj = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("GET");
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String l = null;
+			while (( l=br.readLine()) != null) {
+				stringBuilder.append(l);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return stringBuilder.toString();
+	}
+	public static void main(String[] args) {
+		InvalidWebServiceInvocation csvAPI = new InvalidWebServiceInvocation();
+		String url = "https://www.gocanvas.com/apiv2/images.xml";
+		String username = "test@test.com";
+		String password = "test123";
+		String outputXml=csvAPI.call(url, username, password);
+		System.out.println("xml response: "+outputXml);
+	}
+}
---
+++ Java Code/Simplified/InvalidWebServiceInvocation_Simplified.java
@@ -0,0 +1,27 @@
+package Simplified;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class InvalidWebServiceInvocation_Simplified {
+	public static void main(String[] args) {
+		StringBuilder stringBuilder = new StringBuilder();
+		try {
+			String apiUrl = "https://www.gocanvas.com/apiv2/images.xml?username=test@test.com&password=test123&form_id=1&begin_second=962461752&end_second=1593613752";
+			URL obj = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("GET");
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String l = null;
+			while ((l = br.readLine()) != null) {
+				stringBuilder.append(l);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("xml response: " + stringBuilder);
+	}
+}
